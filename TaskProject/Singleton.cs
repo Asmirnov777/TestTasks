6ece8eb6746b457ed2118777f32936d71db07cca
- <->
2022-10-28 13:44:43 +0300

    Добавлен синглетон.

---
+++ TaskProject/Singleton.cs
@@ -0,0 +1,24 @@
+﻿namespace TaskProject
+{
+    class Singleton
+    {
+        private Singleton() { }
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (locker)
+                {
+                    if (instance == null)
+                        instance = new Singleton();
+                }
+            }
+            return instance;
+        }
+
+        private static Singleton instance;
+
+        private static object locker = new object();
+    }
+}
